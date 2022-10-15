6264bfd363c4e75f75a519b2a3885cbdcfcc8064
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-10-15 13:48:03 +0200

    Lab3 prep

---
+++ Lab3/pop_orderinfo.sql
@@ -0,0 +1,5 @@
+insert into orderinfo(customer_id, date_placed, date_shipped, shipping) values(3,'13-03-2000','17-03-2000', 2.99);
+insert into orderinfo(customer_id, date_placed, date_shipped, shipping) values(8,'23-06-2000','24-06-2000', 0.00);
+insert into orderinfo(customer_id, date_placed, date_shipped, shipping) values(15,'02-09-2000','12-09-2000', 3.99);
+insert into orderinfo(customer_id, date_placed, date_shipped, shipping) values(13,'03-09-2000','10-09-2000', 2.99);
+insert into orderinfo(customer_id, date_placed, date_shipped, shipping) values(8,'21-07-2000','24-07-2000', 0.00);
